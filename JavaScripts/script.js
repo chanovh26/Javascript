c94f7704844ea9bdf9ff39d8220d61afe67d5991
Rodrigo Benitez <135028583+chanovh26@users.noreply.github.com>
2023-08-10 10:48:35 -0300

    Add files via upload

---
+++ JavaScripts/script.js
@@ -0,0 +1,14 @@
+function quieresGalletas(params) {
+    if (confirm("¿Quieres galletitas?")) {
+        //Esto se ejecuta si es true//
+        alert("¿Quieres galletas?")
+        prompt("¿Como estuvieron tus galletas?")
+
+    }
+
+    else {
+        //Esto se ejecuta si es false//
+        alert("Lastima mas para mi")
+    }
+}
+
